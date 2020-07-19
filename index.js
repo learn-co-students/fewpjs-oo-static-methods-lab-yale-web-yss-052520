e2da1f2901b8adb8a647272d6187cb78861700fa
Drew Beckmen <drew.beckmen@yale.edu>
2020-07-19 17:32:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.substr(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^-, 'A-Za-z0-9']+/g, '');
+  }
+
+  static titleize(str) {
+    let words = str.split(" ")
+    const keyWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    for (let i in words) {
+      if ((!keyWords.includes(words[i])) || i == 0) words[i] = this.capitalize(words[i])
+    }
+    return words.join(" ")
+  }
 }
