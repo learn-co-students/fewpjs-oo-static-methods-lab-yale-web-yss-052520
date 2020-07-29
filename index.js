6f59e1b4903faec3cc8a33c2f99c32cd49295de9
tbartelsmeyer <47281160+tbartelsmeyer@users.noreply.github.com>
2020-07-28 22:29:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
-  //add static methods here
+  static capitalize (word) {
+    // const arr = word.split('')
+    // arr[0] = arr[0].toUpperCase()
+    // return arr.join('')
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }
+
+  static sanitize( word ) {
+    return word.replace( /[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(input) {
+    const ignore = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let out = []
+    let wordsArr = input.split(' ')
+    for (let i = 0; i < wordsArr.length; i++) {
+      if (i == 0) {
+        out.push( this.capitalize(wordsArr[i]) )
+      } else if ( ignore.includes(wordsArr[i]) ) {
+        out.push(wordsArr[i])
+      } else {
+        out.push( this.capitalize(wordsArr[i]) )
+      }
+    }
+    return out.join(' ')
+  }
+
 }
